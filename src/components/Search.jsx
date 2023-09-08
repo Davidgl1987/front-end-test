30334070d820c9a2d4c4ce1cc218617e730bd8e1
Davidgl1987 <davidgl1987@gmail.com>
2023-09-08 20:19:22 +0200

    Filtering products

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-export const Search = () => {
+export const Search = ({ onChange }) => {
   return (
     <div className='relative'>
       <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
@@ -6,7 +6,12 @@ export const Search = () => {
           <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z' />
         </svg>
       </div>
-      <input type='text' className='w-full p-4 pl-10 text-sm text-gray-900 rounded-xl shadow-lg bg-gray-50 focus:ring focus:ring-cyan-600 focus:border-cyan-500 focus-visible:outline-none transition-all' placeholder='Acer, Iconia, Alcatel...' required />
+      <input
+        type='text'
+        className='w-full p-4 pl-10 text-sm text-gray-900 rounded-xl shadow-lg bg-gray-50 focus:ring focus:ring-cyan-600 focus:border-cyan-500 focus-visible:outline-none transition-all'
+        placeholder='Acer, Tab, Alcatel...'
+        onChange={({ target }) => onChange(target.value)}
+      />
     </div>
   )
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,12 +1,23 @@
+import { useState, useEffect } from 'react'
 import { Breadcrumbs, ProductItem, Search } from '../components'
 import { useProducts } from '../hooks'
 
 export const ProductListPage = () => {
   const { data: products, isLoading, error } = useProducts()
+  const [filteredProducts, setFilteredProducts] = useState([])
+
+  useEffect(() => {
+    if (products) setFilteredProducts(products)
+  }, [products])
 
   if (isLoading) return <div>loading...</div>
 
   if (error) return <div>Error</div>
+
+  const filterProducts = (word) => {
+    const searchedProducts = products.filter(product => product.brand.toLowerCase().includes(word.toLowerCase()) || product.model.toLowerCase().includes(word.toLowerCase()))
+    setFilteredProducts(searchedProducts)
+  }
 
   return (
     <div className='max-w-screen-xl mx-auto p-4'>
@@ -15,11 +26,11 @@ export const ProductListPage = () => {
           <Breadcrumbs breadcrumbs={[{ name: 'Home' }]} />
         </div>
         <div className='mb-4 col-span-1'>
-          <Search />
+          <Search onChange={filterProducts} />
         </div>
       </div>
       <div className='grid justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-4'>
-        {products.map(product => (
+        {filteredProducts.map(product => (
           <ProductItem key={product.id} product={product} />
         ))}
       </div>

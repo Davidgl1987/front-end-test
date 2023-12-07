9249dd09e5ff034184883455254dd8a9653c2b36
David <dgarcia@nutricontrol.com>
2023-12-07 10:07:02 +0100

    Remove fragment

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -5,15 +5,13 @@ export const CartButton = () => {
   const cart = useStore(state => state.cart)
   const togglePreviewCart = useStore(state => state.togglePreviewCart)
   return (
-    <>
-      <button
-        className='bg-cyan-700 hover:bg-cyan-800 transition-all hover:scale-110 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 relative'
-        type='button'
-        onClick={togglePreviewCart}
-      >
-        <CartIcon className='text-white' />
-        {cart.length > 0 && <div className='absolute inline-flex items-center justify-center w-5 h-5 text-xs text-white bg-red-500 rounded-full -top-2 -right-2'>{cart.length}</div>}
-      </button>
-    </>
+    <button
+      className='bg-cyan-700 hover:bg-cyan-800 transition-all hover:scale-110 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 relative'
+      type='button'
+      onClick={togglePreviewCart}
+    >
+      <CartIcon className='text-white' />
+      {cart.length > 0 && <div className='absolute inline-flex items-center justify-center w-5 h-5 text-xs text-white bg-red-500 rounded-full -top-2 -right-2'>{cart.length}</div>}
+    </button>
   )
 }
